6c57cb17241d496cc9bc7bdcfd004382d7b7c34f
belalkhaleel <belalkhaleel19@gmail.com>
2022-12-11 21:58:16 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,17 @@
+"SELECT name FROM students;"
+"SELECT * 
+FROM students
+WHERE Age > 30;"
+"SELECT name
+FROM students
+WHERE Gender ="F" AND Age = "30";"
+"SELECT Points
+FROM students
+where name = "Alex";"
+"INSERT INTO students VALUES(7, 'Belal', 28, 'M');"
+"UPDATE students
+SET Points = "400"
+WHERE Points = "300";"
+"UPDATE students
+SET Points = "100"
+WHERE Points = "200";"
